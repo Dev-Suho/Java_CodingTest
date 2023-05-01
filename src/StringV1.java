6323fb6fb0d39a421938669f10f93545849f7b00
Dev-Suho <devsuho99@gmail.com>
2023-05-01 20:21:34 +0900

    StringV1
    0501 Commit

---
+++ src/StringV1.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class StringV1 {
+    public static int solution(String str, char t) {
+        int answer = 0;
+        str = str.toUpperCase();
+        t = Character.toUpperCase(t);
+
+        for (char x : str.toCharArray()) {
+            if (x == t) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char c = sc.next().charAt(0);
+
+        int result = solution(str, c);
+
+        System.out.println(result);
+    }
+
+}
