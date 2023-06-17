29ef78911433e2c19c0a17a579b74787dc15ef9d
Dev-Suho <devsuho99@gmail.com>
2023-06-17 21:06:48 +0900

    Baekjoon1546_0617

---
+++ src/programmers/Baekjoon1546.java
@@ -0,0 +1,27 @@
+package programmers;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Baekjoon1546 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = 0;
+        int sum = 0;
+        int max = 0;
+
+        count = sc.nextInt();
+
+        int[] arr = new int[count];
+
+        for(int i = 0; i < count; i++) {
+            arr[i] = sc.nextInt();
+            if (max < arr[i])
+                max = arr[i];
+            sum += arr[i];
+        }
+
+        System.out.println(sum * 100.0/max/count);
+    }
+}
