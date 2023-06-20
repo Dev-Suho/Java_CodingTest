14a5e49a0323289aa48cae739f93c77803c80cb1
Dev-Suho <devsuho99@gmail.com>
2023-06-20 14:17:50 +0900

    Baekjoon2018

---
+++ src/baekjoon/Baekjoon2018.java
@@ -0,0 +1,31 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon2018 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int count = 1;
+        int startPoint = 1;
+        int endPoint = 1;
+        int sum = 1;
+
+        while (endPoint != num) {
+            if(sum == num) {
+                count++;
+                endPoint++;
+                sum = sum + endPoint;
+            } else if (sum > num) {
+                sum = sum - startPoint;
+                startPoint++;
+            } else {
+                endPoint++;
+                sum = sum + endPoint;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
