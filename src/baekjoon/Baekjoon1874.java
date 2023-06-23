a2a3bcedb8d6c529bf4086e5adbb3bc5c1118b97
Dev-Suho <devsuho99@gmail.com>
2023-06-23 12:33:08 +0900

    Create Baekjoon1874.java

---
+++ src/baekjoon/Baekjoon1874.java
@@ -0,0 +1,48 @@
+package baekjoon;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Baekjoon1874 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int length = sc.nextInt();
+        int[] arr = new int[length];
+        for (int i = 0; i < length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Stack<Integer> stack = new Stack<>();
+
+        int num = 1;
+        boolean result = true;
+
+        StringBuffer bf = new StringBuffer();
+
+        for(int i = 0; i < arr.length; i++) {
+            int su = arr[i];
+
+            if(su >= num) {
+                while(su >= num) {
+                    stack.push(num++);
+                    bf.append("+\n");
+                }
+                stack.pop();
+                bf.append("-\n");
+            } else {
+              int front = stack.pop();
+              if (front > su) {
+                  System.out.println("NO");
+                  result = false;
+                  break;
+              } else {
+                  bf.append("-\n");
+              }
+            }
+        }
+
+        if(result) System.out.println(bf);
+    }
+}
