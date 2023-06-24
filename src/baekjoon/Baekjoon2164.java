8c4a3818d49e8f8705f878edb0227f7be9a5feeb
Dev-Suho <devsuho99@gmail.com>
2023-06-24 19:58:55 +0900

    Create Baekjoon2164.java

---
+++ src/baekjoon/Baekjoon2164.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.Queue;
+
+public class Baekjoon2164 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Queue<Integer> myQueue = new LinkedList<>();
+        int num = sc.nextInt();
+        for (int i = 1; i < num+1; i++) {
+            myQueue.add(i);
+        }
+
+        while(myQueue.size() > 1) {
+            myQueue.poll();
+            int poll = myQueue.poll();
+            myQueue.add(poll);
+        }
+        System.out.println(myQueue.poll());
+    }
+}
